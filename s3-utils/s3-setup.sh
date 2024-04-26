eabec8e56e89d451458a4f4150776719c84e2d72
Devon Bautista <devonb@lanl.gov>
2024-04-25 18:38:32 -0600

    Switch from $0 to ${BASH_SOURCE[0]}
    $0 behaves differently when the script is sourced. ${BASH_SOURCE[0]} will return the path of the script properly.

--- s3-utils/s3-setup.sh
+++ s3-utils/s3-setup.sh
@@ -2,4 +2,4 @@
 export S3_URL=http://$DUMMY_IP:9000
 export S3_ACCESS=$S3_ACCESS
 export S3_SECRET=$S3_SECRET
-export PATH=$PATH:$(readlink -f $(dirname $0)/bin/)
+export PATH=$PATH:$(readlink -f $(dirname ${BASH_SOURCE[0]})/bin)
